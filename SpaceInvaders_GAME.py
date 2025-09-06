76bfce96b526b8621a3aeefd3efa1506e0585c65
DiyanAmin <pakuptl3112@gmail.com>
2025-09-06 11:37:37 +0530

    Bullet Not Moving // Added backround image and font

--- SpaceInvaders_GAME.py
+++ SpaceInvaders_GAME.py
@@ -16,7 +16,7 @@ CollisionDistance=27
 # Setup
 g.init()
 Screen=g.display.set_mode((ScreenWidth,ScreenHeight))
-backround=g.image.load('Space_BG.jpeg')
+backround=g.image.load('BG.png')
 g.display.set_caption('Space Invaders V1.01')
 icon=g.image.load('UFO.png')
 g.display.set_icon(icon)
@@ -25,6 +25,7 @@ g.display.set_icon(icon)
 playerImg=g.image.load('PLAYER.png')
 playerX=PlayerStart_X
 playerY=PlayerStart_Y
+playerX_change=0
 
 #Enemy
 enemyImg=[]
@@ -42,3 +43,94 @@ for i in range(EnemyNum):
     enemyY_change.append(EnemySpeed_Y)
 
 #Bullet
+bulletImg=g.image.load('Bullet.png')
+bulletX=0
+bulletY=PlayerStart_Y
+bulletXchange=0
+bulletYchange=BulletSpeed_Y
+bulletState='ready'
+
+#Score
+scoreVal=0
+font=g.font.Font('freesansbold.ttf',64)
+textX=10
+textY=10
+
+#Important functions
+overFont=g.font.Font('freesansbold.ttf',64)
+def showScore(x,y):
+    #Display the current score on screen
+    score=font.render('Score:'+str(scoreVal),True,(255,255,255))
+    Screen.blit(score,(x,y))
+
+def GameOverText():
+    #Display the game over text
+    OverText=overFont.render('GAME OVER',True,(255,255,255))
+    Screen.blit(OverText,(200,250))
+
+def player(x,y):
+    Screen.blit(playerImg,(x,y))
+
+def enemy(x,y,i):
+    Screen.blit(enemyImg[i],(x,y))
+
+def FireBullet(x,y):
+    global bulletState
+    bulletState='fire'
+    Screen.blit(bulletImg,(x+16,y+10))
+
+def isCollision(enemyX,enemyY,bulletX,bulletY):
+    #If bullet hit enemy --> enemy die
+    distance=math.sqrt((enemyX-bulletX)**2+(enemyY-bulletY)**2)
+    return distance<CollisionDistance
+
+#Game Loop
+running=True
+while running:
+    Screen.fill((0,0,0))
+    Screen.blit(backround,(0,0))
+    for event in g.event.get():
+        if event.type==g.QUIT:
+            running=False
+        if event.type==g.KEYDOWN:
+            if event.key==g.K_LEFT:
+                playerX_change=-5
+            if event.key==g.K_RIGHT:
+                playerX_change=5
+            if event.key==g.K_SPACE and bulletState=='ready':
+                bulletX=playerX
+                FireBullet(bulletX,bulletY)
+        if event.type==g.KEYUP and event.key in [g.K_LEFT,g.K_RIGHT]:
+            playerX_change=0
+    #Player Movement
+    playerX+=playerX_change
+    playerX=max(0,min(playerX,ScreenWidth-64))#64 is player size
+
+    #Enemy Movement
+    for i in range(EnemyNum):
+       if enemyY[i]>340: #--> if true, game over
+            for j in range(EnemyNum):
+                enemyY[j]=2000
+            GameOverText()
+            break
+       enemyX[i]+=enemyX_change[i]
+       if enemyX[i]<=0 or enemyX[i]>=ScreenWidth-64:
+           enemyX_change[i]*=-1
+           enemyY[i]+=enemyY_change[i]
+        #Collision Check
+       if isCollision(enemyX[i],enemyY[i],bulletX,bulletY):
+           bulletY=PlayerStart_Y
+           bulletState='ready'
+           scoreVal+=1
+           enemyX[i]=random.randint(0,ScreenWidth-64)
+           enemyY[i]=random.randint(EnemyStart_Ymin,EnemyStart_Ymax)
+       enemy(enemyX[i],enemyY[i],i)
+    #Bullet Movement
+    if bulletY<=0:
+        bulletY=PlayerStart_Y
+        bulletState='ready'
+    elif bulletState=='fire':
+        FireBullet(bulletX,bulletY)
+    player(playerX,playerY)
+    showScore(textX,textY)
+    g.display.update()
